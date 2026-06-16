b3d89800631b57b7de091e5e67f7f6abbebe51a0
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-16 11:45:11 -0500

    Restore CRUD index method contract

--- src/Service/Crud/Operation/CrudIndexOperation.php
+++ src/Service/Crud/Operation/CrudIndexOperation.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace App\Cruding\Service\Crud\Operation;
 
 use App\Cruding\ServiceInterface\Crud\Operation\CrudIndexOperationInterface;
+use App\Cruding\Value\Surface\CrudSurfaceContract;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final class CrudIndexOperation implements CrudIndexOperationInterface
 {
+    public function handle(Request $request): Response|CrudSurfaceContract
+    {
+        return new Response(status: Response::HTTP_NOT_IMPLEMENTED);
+    }
 }
