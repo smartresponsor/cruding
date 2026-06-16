74eb10d277b375a3b0f150960a2624b07219e689
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-16 14:39:11 -0500

    Complete runtime surface token reader move

--- src/Service/Crud/Runtime/CrudRuntimeSurfaceTokenReader.php
+++ src/Service/Crud/Runtime/CrudRuntimeSurfaceTokenReader.php
@@ -6,4 +6,18 @@ namespace App\Cruding\Service\Crud\Runtime;
 
 final readonly class CrudRuntimeSurfaceTokenReader
 {
+    public function __construct(
+        private CrudRuntimeEnvReader $envReader,
+        private CrudRuntimeTokenNormalizer $normalizer,
+        private string $envName,
+    ) {
+    }
+
+    /**
+     * @return list<string>
+     */
+    public function read(): array
+    {
+        return $this->normalizer->csvToTokenList($this->envReader->read($this->envName));
+    }
 }
