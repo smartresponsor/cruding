c0182644c5f45b7c6d6c31ca4810b0f37fd90126
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-16 11:45:42 -0500

    Resolve CRUD index context

--- src/Service/Crud/Operation/CrudIndexOperation.php
+++ src/Service/Crud/Operation/CrudIndexOperation.php
@@ -4,15 +4,28 @@ declare(strict_types=1);
 
 namespace App\Cruding\Service\Crud\Operation;
 
+use App\Cruding\Service\Crud\CrudNotFoundResponseFactory;
+use App\Cruding\ServiceInterface\Crud\CrudContextResolverInterface;
 use App\Cruding\ServiceInterface\Crud\Operation\CrudIndexOperationInterface;
 use App\Cruding\Value\Surface\CrudSurfaceContract;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-final class CrudIndexOperation implements CrudIndexOperationInterface
+final readonly class CrudIndexOperation implements CrudIndexOperationInterface
 {
+    public function __construct(
+        private CrudContextResolverInterface $contextResolver,
+        private CrudNotFoundResponseFactory $notFoundResponseFactory,
+    ) {
+    }
+
     public function handle(Request $request): Response|CrudSurfaceContract
     {
-        return new Response(status: Response::HTTP_NOT_IMPLEMENTED);
+        $context = $this->contextResolver->tryResolve($request);
+        if (null === $context) {
+            return $this->notFoundResponseFactory->create($request, 'crud_context_not_found');
+        }
+
+        return $this->notFoundResponseFactory->create($request, 'crud_index_unavailable');
     }
 }
