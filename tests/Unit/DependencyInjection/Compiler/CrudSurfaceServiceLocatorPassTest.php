2098342b3ba43d1e7098cb17095ad10ff3302d67
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-16 11:54:40 -0500

    Restore locator test dependencies

--- tests/Unit/DependencyInjection/Compiler/CrudSurfaceServiceLocatorPassTest.php
+++ tests/Unit/DependencyInjection/Compiler/CrudSurfaceServiceLocatorPassTest.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace App\Cruding\Tests\Unit\DependencyInjection\Compiler;
 
-final class CrudSurfaceServiceLocatorPassTest
+use App\Cruding\DependencyInjection\Compiler\CrudSurfaceServiceLocatorPass;
+use App\Cruding\Service\Surface\CrudSurfaceServiceLocator;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\Argument\ServiceLocatorArgument;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+final class CrudSurfaceServiceLocatorPassTest extends TestCase
 {
 }
