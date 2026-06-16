2bf2ae5cc6a2f551fa6a8a093a808e875be4607a
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-16 14:36:54 -0500

    Move runtime entity reader into CRUD category

---
+++ src/Service/Crud/Runtime/CrudRuntimeEntityReader.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Cruding\Service\Crud\Runtime;
+
+final readonly class CrudRuntimeEntityReader
+{
+    public function __construct(
+        private CrudRuntimeEnvReader $envReader,
+        private CrudRuntimeTokenNormalizer $normalizer,
+        private string $envName,
+    ) {
+    }
+
+    /**
+     * @return list<string>
+     */
+    public function read(): array
+    {
+        return $this->normalizer->csvToTokenList($this->envReader->read($this->envName));
+    }
+}
