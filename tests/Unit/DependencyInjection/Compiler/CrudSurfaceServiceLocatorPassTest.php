786f4ed3e2aaebcc86a66c1fe7f57895330d972d
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-16 11:55:09 -0500

    Test canonical locator service collection

--- tests/Unit/DependencyInjection/Compiler/CrudSurfaceServiceLocatorPassTest.php
+++ tests/Unit/DependencyInjection/Compiler/CrudSurfaceServiceLocatorPassTest.php
@@ -13,4 +13,22 @@ use Symfony\Component\DependencyInjection\Definition;
 
 final class CrudSurfaceServiceLocatorPassTest extends TestCase
 {
+    public function testCollectsCanonicalHostAndComponentServices(): void
+    {
+        $container = new ContainerBuilder();
+        $container->setDefinition(CrudSurfaceServiceLocator::class, new Definition(CrudSurfaceServiceLocator::class));
+        $container->setDefinition('App\\Service\\Http\\Host\\HostIndexService', new Definition('App\\Service\\Http\\Host\\HostIndexService'));
+        $container->setDefinition('component.document.index', new Definition('App\\Fixture\\Service\\Http\\Document\\DocumentIndexService'));
+        $container->setDefinition('App\\Something\\ElseService', new Definition('App\\Something\\ElseService'));
+
+        (new CrudSurfaceServiceLocatorPass())->process($container);
+
+        $argument = $container->getDefinition(CrudSurfaceServiceLocator::class)->getArgument(0);
+        self::assertInstanceOf(ServiceLocatorArgument::class, $argument);
+
+        $values = $argument->getValues();
+        self::assertArrayHasKey('App\\Service\\Http\\Host\\HostIndexService', $values);
+        self::assertArrayHasKey('component.document.index', $values);
+        self::assertArrayNotHasKey('App\\Something\\ElseService', $values);
+    }
 }
